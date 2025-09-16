199f7f9fc7158c285ea34d4aeb4cae48ff7680dc
Kavya096051 <096051@rkcrajkot.in>
2025-09-16 15:55:38 +0530

    3

---
+++ main (2).c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+
+{
+
+int a,sq,cub;
+
+
+printf("enter a value : ");
+scanf("%d",&a);
+
+sq= a*a;
+cub= a*a*a;
+
+printf("\n square of guven value is : %d ",sq);
+printf("\n cube of given value is : %d ",cub);
+
+getch();
+
+}
+
+
+
+
+
+
+
+
+
+

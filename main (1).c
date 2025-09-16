c13df1030f8f9ffe5997d24040f7b55f252ae038
Kavya096051 <096051@rkcrajkot.in>
2025-09-16 15:52:51 +0530

    Add files via upload

---
+++ main (1).c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+
+{
+    float a,b;
+
+    printf("enter value of a: ");
+    scanf("%f",&a);
+
+    printf("enter value of b: ");
+    scanf("%f",&b);
+    
+
+    printf("\n sum of a & b is : %f ",a+b);
+    printf("\n diff of a & b is : %f ",a-b);
+    printf("\n multi of a & b is : %f ",a*b);
+    printf("\n div of a & b is : %f ",a/b);
+  
+
+    getch();
+
+}
+
+
+
+

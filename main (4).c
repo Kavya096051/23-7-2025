66b51edf657077246f5d05ea72d9e58c53a40ca6
Kavya096051 <096051@rkcrajkot.in>
2025-09-16 15:58:50 +0530

    Add files via upload

---
+++ main (4).c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+
+{
+
+    float b,h,T;
+
+    printf("enter value of base length : ");
+    scanf("%f",&b);
+
+    printf("enter value of height length : ");
+    scanf("%f",&h);
+
+
+    T=b*h/2;
+
+    printf("\n area of triangle is : %f",T);
+
+    getch();
+
+}
+
+
+
+
+
+
+

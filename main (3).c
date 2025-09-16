bca7af810549471348de9e41f352d799a04bf57c
Kavya096051 <096051@rkcrajkot.in>
2025-09-16 15:57:28 +0530

    Add files via upload

---
+++ main (3).c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+
+{
+
+    float pi,r,A;
+
+    pi=3.14;
+
+    printf("enter value of radius : ");
+    scanf("%f",&r);
+
+    A= pi*r*r;
+    
+    printf("\n area of circle is : %f",A);
+
+
+    getch();
+    
+}
+
+
+
+
+
+

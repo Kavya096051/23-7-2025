c3f743c06271b7719c0bab9686ae412de1d655a9
Kavya096051 <096051@rkcrajkot.in>
2025-09-16 16:02:54 +0530

    Add files via upload

---
+++ main (7).c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+
+{
+
+    int a;
+
+    printf("enter a value to check : ");
+    scanf("%d",&a);
+
+    if(a>0)
+    
+    { 
+        printf("\nvalue is positive");
+    }
+
+    else
+    {
+        printf("\nvalue is negative");
+    }
+
+  
+    getch();
+    
+}
+
+
+
+
+
+
+
